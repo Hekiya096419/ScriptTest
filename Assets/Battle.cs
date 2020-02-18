d41374a3eb964f6770ddda39d41200cfd409b6db
cheng096419 <hekiya096419@hotmail.com>
2020-02-18 21:10:39 +0900

    ScriptTest Lesson4課題
    配列宣言（Test）＆Bossクラス（Battle）

---
+++ Assets/Battle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+	private int mp = 53;
+	public void Magic(){
+		if (mp > 4){
+			this.mp -= 5;
+			Debug.Log("魔法攻撃した。残りMP" + mp + "。");
+		}else{
+			Debug.Log("MPが足りないため魔法が使えない。");
+		}
+	}
+}
+
+public class Battle : MonoBehaviour{
+	void Start () {
+		Boss Boss = new Boss();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+		Boss.Magic();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//要素数5の配列を初期化する
+		int[] points = {560, 68, 9873, 23, 784};
+		//配列の要素数のぶんだけ処理を繰り返す
+		for (int i = 0; i < points.Length; i++){
+			Debug.Log (points [i]);
+		}
+		for (int i = 4; i >= 0; i--) {
+			Debug.Log (points [i]);
+		}
+	}
+		// Update is called once per frame
+	void Update () {
+		
+	}
+}
